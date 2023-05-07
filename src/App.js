06f61a98bea07ecbcb797c5f82e96301fd794def
KingKyoTrue <tanadicoding@gmail.com>
2023-05-08 01:13:15 +0700

    finish tailwind

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import logo from './logo.svg';
+import "@fontsource/poppins/400.css"
+import "@fontsource/poppins/600.css"
+import "@fontsource/poppins/700.css"
+import "@fontsource/merriweather/400.css"
+import "@fontsource/merriweather/700.css"
 import './App.css';
 
 function App() {
@@ -6,6 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p className=" font-serif text-red-700 text-4xl"> Hello World</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
